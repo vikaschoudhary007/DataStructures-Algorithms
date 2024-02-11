96e0597ee7a39e47acad914693c16c243cf9a9d5
vikaschoudhary007 <vikaschoudhary572001@gmail.com>
2024-02-10 20:41:20 -0500

    Day 19

--- Leetcode/1043. Partition Array for Maximum Sum.java
+++ Leetcode/1043. Partition Array for Maximum Sum.java
@@ -1,24 +1,23 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
 
-        int[] dp = new int[arr.length];
+        int[] dp = new int[arr.length+1];
 
-        if(arr.length == 1){
-            return arr[0];
-        }else if(arr.length == 2){
-            return Math.max(arr[0], arr[1])*2;
+        for(int i=1; i<= arr.length; i++){
+
+            int maxSum = 0;
+            int currentSum = 0;
+
+            for(int j = 1; j <= k && j <= i; j++){
+
+                currentSum = Math.max(currentSum, arr[i-j]);
+                maxSum = Math.max(maxSum, dp[i-j] + currentSum * j);
+            }
+
+            dp[i] = maxSum;
         }
 
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0], arr[1])*2;
-        dp[2] = Math.max(Math.max(arr[0], arr[1]), arr[2])*3;
-
-        for(int i = 3; i < arr.length; i++){
-
-            dp[i] = Math.max(dp[i-1], arr[i])*i;
-        }
-
-        return dp[arr.length-1];
+        return dp[arr.length];
 
     }
 }
