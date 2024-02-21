10ada6613633c2f4f581d3a2de320b177c245f11
vikaschoudhary007 <vikaschoudhary572001@gmail.com>
2024-02-20 21:39:43 -0500

    Day 26 - used priority queue

--- Leetcode/1642. Furthest Building You Can Reach.java
+++ Leetcode/1642. Furthest Building You Can Reach.java
@@ -1,37 +1,59 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
 
-        return helper(heights, bricks, ladders, 0);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        for(int i = 0; i< heights.length - 1; i++){
+
+            int heightDiff = heights[i+1] - heights[i];
+
+            if(heightDiff > 0){
+
+                pq.offer(heightDiff);
+
+                if(pq.size() > ladders){
+                    bricks = bricks - pq.poll();
+                }
+
+                if(bricks < 0){
+                    return i;
+                }
+            }
+        }
+
+        return heights.length - 1;
+
+        // return helper(heights, bricks, ladders, 0);
     }
 
-    public int helper(int[] heights, int bricksRem, int laddersRem, int currIndex){
+    // public int helper(int[] heights, int bricksRem, int laddersRem, int currIndex){
 
 
-        if(currIndex == heights.length - 1){
-            return currIndex;
-        }
+    //     if(currIndex == heights.length - 1){
+    //         return currIndex;
+    //     }
 
-        if(heights[currIndex+1] <= heights[currIndex]){
-            return helper(heights, bricksRem, laddersRem, currIndex+1);
-        }
+    //     if(heights[currIndex+1] <= heights[currIndex]){
+    //         return helper(heights, bricksRem, laddersRem, currIndex+1);
+    //     }
 
-        if(bricksRem == 0 && laddersRem == 0){
-            return currIndex;
-        }
+    //     if(bricksRem == 0 && laddersRem == 0){
+    //         return currIndex;
+    //     }
 
-        if(bricksRem < (heights[currIndex+1] - heights[currIndex]) && laddersRem == 0){
-            return currIndex;
-        }
+    //     if(bricksRem < (heights[currIndex+1] - heights[currIndex]) && laddersRem == 0){
+    //         return currIndex;
+    //     }
 
 
-        if(bricksRem >= 0 && laddersRem >= 0){
-            return Math.max(
-                    helper(heights, bricksRem - (heights[currIndex+1] - heights[currIndex]),
-                            laddersRem, currIndex+1),
-                    helper(heights, bricksRem, laddersRem-1, currIndex+1)
-            );
-        }else{
-            return currIndex;
-        }
-    }
+    //     if(bricksRem >= 0 && laddersRem >= 0){
+    //         return Math.max(
+    //             helper(heights, bricksRem - (heights[currIndex+1] - heights[currIndex]),
+    //             laddersRem, currIndex+1),
+    //             helper(heights, bricksRem, laddersRem-1, currIndex+1)
+    //         );
+    //     }else{
+    //         return currIndex;
+    //     }
+    // }
 }
